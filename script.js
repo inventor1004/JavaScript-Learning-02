25a4a6958c29c9fde7448bff099777fc60fd3415
inventor1004 <117550785+inventor1004@users.noreply.github.com>
2023-09-23 16:21:35 -0400

    Fundamental of the object in JsScript and how to apply it.

--- script.js
+++ script.js
@@ -36,3 +36,45 @@ console.log(friends.indexOf('Steven')); // 1
 // Check whether the certain element is in array or not
 console.log(friends.includes('Steven')); // ture
 console.log(friends.includes('Chris'));  // false
+
+
+
+/* Objects */
+const person = {
+  firstName: 'Chris',
+  lastName: 'Park',
+  age: 27,
+  nation: 'Korea',
+  job: 'Student'
+}
+
+// using ${}
+console.log(`My name is ${person.firstName} ${person.lastName}\n
+I'm ${person.age} years old and from ${person.nation}.\n
+Now I'm a ${person.job}.`)
+
+// using ['']
+console.log(`My name is ${person['firstName']} ${person['lastName']}\n
+I'm ${person['age']} years old and from ${person['nation']}.\n
+Now I'm a ${person['job']}.`)
+// same output:
+// My name is Chris Park
+// I'm 27 years old and from Korea.
+// Now I'm a Student.
+
+
+// Using bracket notion
+// to cobcatebate the key using variable
+const nameKey = 'Name'
+console.log(person['first' + nameKey] + ' ' + person['last' + nameKey]); // Chris Park
+//> dot notation can not cobcatedates mulitple keys
+
+
+// Apply object
+// prompt to the user and answer based on the user input
+const userInput = prompt('What do you want to know about Chris? Choose between firstName, lastName, age, job, or nation!');
+
+if (person[userInput])
+  alert(`His ${userInput} is ${person[userInput]}`);
+else
+  alert("Sorry. I don't have that information...");
